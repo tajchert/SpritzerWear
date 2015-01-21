2fab7f1d4174d7f2a4b1776d65e13d56248c7c2f
Michal Tajchert <michal.tajchert@polidea.com>
2015-01-21 01:18:14 +0100

    Text size and playing listeners.

--- SpritzerWear/wear/src/main/java/pl/tajchert/spritzerwear/MainActivity.java
+++ SpritzerWear/wear/src/main/java/pl/tajchert/spritzerwear/MainActivity.java
@@ -3,11 +3,17 @@ package pl.tajchert.spritzerwear;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
-import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.andrewgiang.textspritzer.lib.Spritzer;
+import com.andrewgiang.textspritzer.lib.SpritzerTextView;
 
 public class MainActivity extends Activity {
-
-    private TextView mTextView;
+    private SpritzerTextView spritzerTV;
+    private LinearLayout linearLayout;
+    private boolean isPlaying;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,41 @@ public class MainActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                spritzerTV = (SpritzerTextView) findViewById(R.id.spritzTV);
+                linearLayout = (LinearLayout) findViewById(R.id.layoutMain);
+                linearLayout.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if(isPlaying){
+                            spritzerTV.pause();
+                        } else {
+                            spritzerTV.play();
+                        }
+                        return false;
+                    }
+                });
+                spritzerTV.setSpritzText("add the spritz text here");
+                setSpritzer();
+            }
+        });
+    }
+
+    private void setSpritzer() {
+        spritzerTV.setOnClickControlListener(new SpritzerTextView.OnClickControlListener() {
+            @Override
+            public void onPause() {
+                isPlaying = false;
+            }
+
+            @Override
+            public void onPlay() {
+                isPlaying = true;
+            }
+        });
+        spritzerTV.setOnCompletionListener(new Spritzer.OnCompletionListener() {
+            @Override
+            public void onComplete() {
+                isPlaying = false;
             }
         });
     }
