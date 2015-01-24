470623b95aea30ec73a7cf5bfd80ccf163a1df2c
Michal Tajchert <mtajchert@gmail.com>
2015-01-24 03:12:06 +0100

    FAB added, and dialog for adding.

--- SpritzerWear/mobile/src/main/java/pl/tajchert/spritzerwear/MainActivity.java
+++ SpritzerWear/mobile/src/main/java/pl/tajchert/spritzerwear/MainActivity.java
@@ -1,9 +1,18 @@
 package pl.tajchert.spritzerwear;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+
+import com.getbase.floatingactionbutton.AddFloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -14,7 +23,7 @@ public class MainActivity extends ActionBarActivity {
 
     private RecyclerView commentsRecList;
     private RecyclerView.Adapter adapter;
-    //private AddFloatingActionButton fab;
+    private AddFloatingActionButton fab;
     private ArrayList<Story> arrayListStories;
 
     @Override
@@ -24,7 +33,13 @@ public class MainActivity extends ActionBarActivity {
 
 
         commentsRecList = (RecyclerView) findViewById(R.id.storyList);
-        //fab = (AddFloatingActionButton) findViewById(R.id.normal_plus);
+        fab = (AddFloatingActionButton) findViewById(R.id.normal_plus);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAddDialog();
+            }
+        });
 
         commentsRecList.setHasFixedSize(false);
         LinearLayoutManager llm = new LinearLayoutManager(this);
@@ -35,6 +50,41 @@ public class MainActivity extends ActionBarActivity {
         arrayListStories.add(new Story("titleTwo", "coooooooooontent two"));
         adapter = new AdapterComment(arrayListStories);
         commentsRecList.setAdapter(adapter);
+    }
+
+    private void showAddDialog(){
+        LinearLayout layout = new LinearLayout(MainActivity.this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        TableLayout.LayoutParams params = new TableLayout.LayoutParams();
+        params.setMargins(20, 20, 20, 0);
+
+        final EditText titleBox = new EditText((MainActivity.this));
+        titleBox.setHint("Title");
+        titleBox.setLayoutParams(params);
+        layout.addView(titleBox);
+
+        final EditText descriptionBox = new EditText((MainActivity.this));
+        descriptionBox.setHint("Description");
+        descriptionBox.setLayoutParams(params);
+        layout.addView(descriptionBox);
+
+
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("New Note").setView(layout).setPositiveButton("Add",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        Story story = new Story(titleBox.getText().toString(), descriptionBox.getText().toString());
+                        if(arrayListStories != null && adapter != null){
+                            arrayListStories.add(story);
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                }).setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                });
+        alert.show();
     }
 
     @Override
